81d5b5e6b128217bf9ec494717d07b7c9f3c088f
CaroCarrillo <carolina.carrillo960120@gmail.com>
2022-04-09 21:01:52 -0500

    update NavbarCaro

--- src/NavbarCaro/index.jsx
+++ src/NavbarCaro/index.jsx
@@ -15,13 +15,13 @@ const NavbarCaro = () => {
             <nav class="navbar" style={{backgroundColor: "#0C345C"}}>
                 <div class="container-fluid">
                     <div className='d-flex justify-content-start'>
-                        <a href="/#" aria-labelledby='Home'><AiOutlineHome /></a> &nbsp; &nbsp; &nbsp;
                         <a aria-current="page" href="/#" ><RiFileList2Line/> Planeación </a>&nbsp; &nbsp;
                         <Link to="/Cursos"> <MdArrowRight />&nbsp; &nbsp;<HiOutlineDocumentDuplicate /> Datos profesionales</Link>
                     </div>
-                    <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search"/>
-                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    <form class="d-flex ">
+                        <a href="/#" aria-labelledby='Home' ><AiOutlineHome /></a>
+                        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar"/>
+                        <button class="btn btn-outline-success" type="submit">Buscar</button>
                     </form>
                 </div>
             </nav>
