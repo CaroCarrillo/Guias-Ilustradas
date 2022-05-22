715c6037643f37e34440bce632f685532ac28b47
Isay Castañeda Torres <salvador.castaneda@MacBook-Pro-de-Isay.local>
2022-05-22 16:45:23 -0500

    Changes in login and register was added, also the connection was stablished with backend

--- src/FormRecoverPassword/index.jsx
+++ src/FormRecoverPassword/index.jsx
@@ -1,9 +1,14 @@
 import {useState} from 'react'
 import FormInput from '../FormInput/index.jsx'
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import "./index.css"
 
 const FormRecoverPassword = () => {
-
+    const [emailSended, setEmailSended] = useState("hidden");
+    const [colorSelected, setColoSelected] =  useState(false);
+    const [emailTexto, setEmailTexto] =  useState("");
+    const navigate = useNavigate();
 const [values,setValues] = useState({
 email:"",
 });
@@ -21,8 +26,32 @@ const inputs =[
 ]
 
 const handleSubmit = (e) => {
+    
     e.preventDefault();
-    console.log(values);
+    const opts = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(values)
+    }
+    fetch("http://127.0.0.1:5000/resetPassword", opts)
+        .then(resp => { return resp.json() })
+        .then(resp => {
+            if (resp.status === 200 && resp.email === "succesReset") {
+                setEmailSended("visible") 
+                setEmailTexto("Email enviado correctamente")
+                setColoSelected(true)
+            }
+            if (resp.status === 200 && resp.email === "NotValid") {
+                setEmailSended("visible") 
+                setEmailTexto("Email no valido")
+                setColoSelected(false)
+            }
+        })
+        .catch(e => {
+            console.log(e)
+        })
 };
 
 const onChange = (e) => {
@@ -36,10 +65,17 @@ const onChange = (e) => {
                 {inputs.map((input) => (
                     <FormInput key={input.id} {...input} value={values[input.name]} onChange={onChange}/>
                 ))}
-                {/*<button onClick={handleSubmit} >Enviar</button>*/}
-                <a href="/OlvideContrasena2" data-toggle="OlvideContrasena2" type='button'>Enviar</a>
+                <button onClick={handleSubmit} >Enviar</button>
+                <div style={{color: colorSelected ? "blue" : "red",
+                             visibility:emailSended}}>
+                    { emailTexto }
+                </div>
+                <a href="./Ingresar"> sing in</a>
             </form>
+            
+
         </div>
+
     );
 };
 
--- src/FormSignin/index.jsx
+++ src/FormSignin/index.jsx
@@ -1,4 +1,5 @@
 import {useState} from 'react'
+import { useNavigate } from 'react-router-dom';
 import FormInput from '../FormInput'
 //import "./login2.css"
 import cuceiBlack from '../assets/images/cuceiBlack.png'
@@ -6,110 +7,135 @@ import udg from '../assets/images/udg.png'
 import './index.css'
 
 const FormSignin = () => {
+    const [existingUser, setExistingUser] = useState("hidden");
+    const navigate = useNavigate();
+    const [values, setValues] = useState({
+        code: "",
+        name: "",
+        lastname: "",
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
 
-const [values,setValues] = useState({
-code:"",
-name:"",
-lastname:"",
-username:"", 
-email:"",
-password:"",
-confirmPassword:"",
-});
+    const inputs = [
+        {
+            id: 1,
+            name: "code",
+            type: "text",
+            placeholder: "Código",
+            errorMessage: "El código debe ser de 9 números",
+            label: "Código",
+            pattern: "^[0-9]{9}$",
+            required: true,
+        },
+        {
+            id: 2,
+            name: "name",
+            type: "text",
+            placeholder: "Nombre(s)",
+            label: "Nombre(s)",
+            required: true,
+        },
+        {
+            id: 3,
+            name: "lastname",
+            type: "text",
+            placeholder: "Apellidos",
+            label: "Apellidos",
+            required: true,
+        },
+        {
+            id: 4,
+            name: "username",
+            type: "text",
+            placeholder: "Nombre de usuario",
+            errorMessage: "El usuario debe ser entre 3-16 caracteres y no debe tener caracteres especiales",
+            label: "Nombre de usuario",
+            pattern: "^[A-Za-z0-9]{3,16}$",
+            required: true,
+        },
+        {
+            id: 5,
+            name: "email",
+            type: "email",
+            placeholder: "Correo institucional",
+            errorMessage: "Debe ser un email válido",
+            label: "Correo institucional",
+            required: true,
+        },
+        {
+            id: 6,
+            name: "password",
+            type: "password",
+            placeholder: "Contraseña",
+            errorMessage: "La contraseña debe tener entre 8-20 caracteres e incluir al menos una letra, un número y un caracter especial",
+            label: "Contraseña",
+            pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+            required: true,
+        },
+        {
+            id: 7,
+            name: "confirmPassword",
+            type: "password",
+            placeholder: "Confirmar contraseña",
+            errorMessage: "La contraseña no coincide",
+            label: "Confirmar contraseña",
+            pattern: values.password,
+            required: true,
+        }
+    ]
 
-const inputs =[
-    {
-    id:1,
-    name:"code",
-    type:"text",
-    placeholder:"Código",
-    errorMessage:"El código debe ser de 9 números",
-    label:"Código",
-    pattern: "^[0-9]{9}$",
-    required: true,
-    },
-    {
-    id:2,
-    name:"name",
-    type:"text",
-    placeholder:"Nombre(s)",
-    label:"Nombre(s)",
-    required: true,
-    },
-    {
-    id:3,
-    name:"lastname",
-    type:"text",
-    placeholder:"Apellidos",
-    label:"Apellidos",
-    required: true,
-    },
-    {
-    id:4,
-    name:"username",
-    type:"text",
-    placeholder:"Nombre de usuario",
-    errorMessage:"El usuario debe ser entre 3-16 caracteres y no debe tener caracteres especiales",
-    label:"Nombre de usuario",
-    pattern: "^[A-Za-z0-9]{3,16}$",
-    required: true,
-    },
-    {
-    id:5,
-    name:"email",
-    type:"email",
-    placeholder:"Correo institucional",
-    errorMessage:"Debe ser un email válido",
-    label:"Correo institucional",
-    required: true,
-    },
-    {
-    id:6,
-    name:"password",
-    type:"password",
-    placeholder:"Contraseña",
-    errorMessage:"La contraseña debe tener entre 8-20 caracteres e incluir al menos una letra, un número y un caracter especial",
-    label:"Contraseña",
-    pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
-    required: true,
-    },
-    {
-    id:7,
-    name:"confirmPassword",
-    type:"password",
-    placeholder:"Confirmar contraseña",
-    errorMessage:"La contraseña no coincide",
-    label:"Confirmar contraseña",
-    pattern: values.password,
-    required: true,
-    }
-]
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const opts = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(values)
+        }
+        fetch("http://127.0.0.1:5000/register", opts)
+            .then(resp => { return resp.json() })
+            .then(resp => {
+                if (resp.status === 200 && resp.user === "created_success") {
+                    
+                    navigate('/Ingresar');
+                }
+                if (resp.status === 200 && resp.user === "occuped") {
+                    setExistingUser("visible")
+                }
+            })
+            .catch(e => {
+                console.log(e)
+            })
+    };
 
-const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(values);
-};
+    const onChange = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+    };
 
-const onChange = (e) => {
-    setValues({...values,[e.target.name]: e.target.value });
-};
 
-    
     return (
         <div className='containerSignin'>
             <div className='headerForm'>
                 <img src={udg} alt="" />
                 <img src={cuceiBlack} alt="" />
                 <h1>Formulario de registro</h1>
-            </div> 
+            </div>
             <form>
                 {inputs.map((input) => (
-                    <FormInput key={input.id} {...input} value={values[input.name]} onChange={onChange}/>
+                    <FormInput key={input.id} {...input} value={values[input.name]} onChange={onChange} />
                 ))}
             </form>
             <div className='buttonForm'>
-                {/*<button onClick={handleSubmit} >Enviar</button>*//*Se utilizará una vez que el back-end esté listo para conectarse al front-end*/}
-                <a href="/Ingresar" data-toggle="Ingresar" type='button'>Enviar</a>
+                <button onClick={handleSubmit} >Enviar</button>
+                <div style={{color:"red",
+                             visibility:existingUser}}>
+                    El correo o el codigo ya se encuentra en uso
+                </div>
+                {/*<a href="/Ingresar" data-toggle="Ingresar" type='button'>Enviar</a>*/}
             </div>
         </div>
     );
